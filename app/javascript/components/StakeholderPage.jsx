25abc73fa8ae3760aacf9bbfa9df8a30950f8fc1
Conor McManus <conor.mcmanus@sendoso.com>
2022-01-05 23:23:15 +0000

    Add improved sidebar

--- app/javascript/components/StakeholderPage.jsx
+++ app/javascript/components/StakeholderPage.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 import Dashboard from './Dashboard';
 
 const StakeholderPage = (props) => {
   return (
-    <Dashboard signOutPath={props.signOutPath}>
+    <Dashboard
+      signOutPath={props.signOutPath}
+      sidebar={[
+        ['Dashboard', <HomeOutlinedIcon />],
+        ['Products', <CreateOutlinedIcon />]
+      ]}>
 
     </Dashboard>
   );
--- app/javascript/components/VendorPage.jsx
+++ app/javascript/components/VendorPage.jsx
@@ -1,11 +1,23 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 import Dashboard from './Dashboard';
 
 const VendorPage = (props) => {
   return (
-    <Dashboard signOutPath={props.signOutPath}>
+    <Dashboard
+      signOutPath={props.signOutPath}
+      sidebar={[
+        ['Dashboard', <HomeOutlinedIcon />],
+        ['Clients', <BusinessOutlinedIcon />],
+        ['Vendors', <StorefrontOutlinedIcon />]
+      ]}>
 
     </Dashboard>
   );
