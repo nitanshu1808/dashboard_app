ebaedd7776374a6f0e5454051eb3850df59c1e71
MassimilianoCavinato <massi304477@live.it>
2022-01-05 14:44:30 +0000

    pulled latest master

---
+++ app/javascript/packs/bundle.js
@@ -0,0 +1,6 @@
+import ReactOnRails from 'react-on-rails';
+import Chart from '../components/Chart'
+
+ReactOnRails.register({
+  Chart,
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_125906) do
-
-  create_table "addresses", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2022_01_04_230810) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
