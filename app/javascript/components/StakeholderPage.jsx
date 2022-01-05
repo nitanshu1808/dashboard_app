b9358a40c5a186f608077b970fef59b21e27c1ab
Conor McManus <conor.mcmanus@sendoso.com>
2022-01-05 23:41:51 +0000

    Add example panels

--- app/javascript/components/StakeholderPage.jsx
+++ app/javascript/components/StakeholderPage.jsx
@@ -2,6 +2,10 @@ import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 import Dashboard from './Dashboard';
 
@@ -13,7 +17,15 @@ const StakeholderPage = (props) => {
         ['Dashboard', <HomeOutlinedIcon />],
         ['Products', <CreateOutlinedIcon />]
       ]}>
-
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper sx={{ p: 2 }}>
+              <Typography component="h2" variant="h6" color="primary">Example Panel</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </Dashboard>
   );
 }
--- app/javascript/components/VendorPage.jsx
+++ app/javascript/components/VendorPage.jsx
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
-
-import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 import Dashboard from './Dashboard';
 
@@ -18,7 +19,15 @@ const VendorPage = (props) => {
         ['Clients', <BusinessOutlinedIcon />],
         ['Vendors', <StorefrontOutlinedIcon />]
       ]}>
-
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper sx={{ p: 2 }}>
+              <Typography component="h2" variant="h6" color="primary">Example Panel</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </Dashboard>
   );
 }
