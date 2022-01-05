faee88a0c76fa9b5e809bec6414707a9b11310f0
Nitanshu Rehani <nitanshurehani@Nitanshus-MacBook-Pro.local>
2022-01-05 14:03:51 +0000

    - removed the invalid index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_230810) do
+ActiveRecord::Schema.define(version: 2022_01_05_125906) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -21,7 +26,6 @@ ActiveRecord::Schema.define(version: 2022_01_04_230810) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
