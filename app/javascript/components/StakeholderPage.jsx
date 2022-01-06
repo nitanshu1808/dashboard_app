b7a607da674cae6c9ac9f00474d199c38b77e5d8
Conor McManus <conor.mcmanus@sendoso.com>
2022-01-06 10:58:12 +0000

    Add ThemeProvider to both dashboards

--- app/javascript/components/StakeholderPage.jsx
+++ app/javascript/components/StakeholderPage.jsx
@@ -4,28 +4,34 @@ import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 import Dashboard from './Dashboard';
 import Panel from './Panel';
 
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 const StakeholderPage = (props) => {
   return (
-    <Dashboard
-      signOutPath={props.signOutPath}
-      sidebar={[
-        ['Dashboard', <HomeOutlinedIcon />],
-        ['Products', <CreateOutlinedIcon />]
-      ]}>
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={12} lg={12}>
-            <Panel title="Example Panel">
+    <ThemeProvider theme={theme}>
+      <Dashboard
+        signOutPath={props.signOutPath}
+        sidebar={[
+          ['Dashboard', <HomeOutlinedIcon />],
+          ['Products', <CreateOutlinedIcon />]
+        ]}>
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={12} lg={12}>
+              <Panel title="Example Panel">
 
-            </Panel>
+              </Panel>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-    </Dashboard>
+        </Container>
+      </Dashboard>
+    </ThemeProvider>
   );
 }
 
--- app/javascript/components/VendorPage.jsx
+++ app/javascript/components/VendorPage.jsx
@@ -5,29 +5,35 @@ import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 import Dashboard from './Dashboard';
 import Panel from './Panel';
 
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 const VendorPage = (props) => {
   return (
-    <Dashboard
-      signOutPath={props.signOutPath}
-      sidebar={[
-        ['Dashboard', <HomeOutlinedIcon />],
-        ['Clients', <BusinessOutlinedIcon />],
-        ['Vendors', <StorefrontOutlinedIcon />]
-      ]}>
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={12} lg={12}>
-            <Panel title="Example Panel">
+    <ThemeProvider theme={theme}>
+      <Dashboard
+        signOutPath={props.signOutPath}
+        sidebar={[
+          ['Dashboard', <HomeOutlinedIcon />],
+          ['Clients', <BusinessOutlinedIcon />],
+          ['Vendors', <StorefrontOutlinedIcon />]
+        ]}>
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={12} lg={12}>
+              <Panel title="Example Panel">
 
-            </Panel>
+              </Panel>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-    </Dashboard>
+        </Container>
+      </Dashboard>
+    </ThemeProvider>
   );
 }
 
